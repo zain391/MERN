ebf8991d475ea2cdafe3d836d84b6dfcce113438
zain391 <zainqamar391@gmail.com>
2022-09-01 21:29:54 +0500

    still working on the call back functions

--- softwarehous/callBackFunction/callBackFunction.js
+++ softwarehous/callBackFunction/callBackFunction.js
@@ -1,11 +1,28 @@
-function person1(){
-    setTimeout(() => {
-        console.log("website layout is loaded");
-        person2()
-    }, 3000);
-}
-function person2(){
-    console.log("data from DB is being loaded");
-}
-person1()
+function myDisplayer(some) {
+    console.log(some);
+  }
+  
+  function myFirst() {
+    myDisplayer("Hello");
+  }
+  
+  function mySecond() {
+    myDisplayer("Goodbye");
+  }
+  
+  mySecond();
+  myFirst();
+//   the first result should be Goodbye
+//   the second result should be Hello
 
+// these are the callback functions 
+// the execution of a function does not depends on the place where it is defined rahter than on the place where it is going to be called 
+
+// settimeout is the best example of callback function 
+const date=new Date();
+function currentDate(){
+    console.log(date.getHours,":",date.getSeconds,":");
+    // setTimeout(() => {
+    //     currentDate()
+    // }, 1000);
+}
