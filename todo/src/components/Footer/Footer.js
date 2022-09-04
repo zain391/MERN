2211ef9ae4d0596827397d1114342c47e525497c
zain391 <zainqamar391@gmail.com>
2022-09-04 22:55:20 +0500

    added the tailwind css in the project

--- todo/src/components/Footer/Footer.js
+++ todo/src/components/Footer/Footer.js
@@ -2,133 +2,124 @@ import React from 'react'
 import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css"
 function Footer() {
     return (
-                <div>
-                <div class="container my-5">
-
-                    <footer
-                        class="text-center text-lg-start text-white"
-                        style="background-color: #1c2331"
-                    >
-                        <section
-                            class="d-flex justify-content-between p-4"
-                            style="background-color: #6351ce"
-                        >
-                            <div class="me-5">
-                                <span>Get connected with us on social networks:</span>
-                            </div>
-
-                            <div>
-                                <a href="" class="text-white me-4">
-                                    <i class="fab fa-facebook-f"></i>
-                                </a>
-                                <a href="" class="text-white me-4">
-                                    <i class="fab fa-twitter"></i>
-                                </a>
-                                <a href="" class="text-white me-4">
-                                    <i class="fab fa-google"></i>
-                                </a>
-                                <a href="" class="text-white me-4">
-                                    <i class="fab fa-instagram"></i>
-                                </a>
-                                <a href="" class="text-white me-4">
-                                    <i class="fab fa-linkedin"></i>
-                                </a>
-                                <a href="" class="text-white me-4">
-                                    <i class="fab fa-github"></i>
-                                </a>
-                            </div>
-                        </section>
-
-                        <section class="">
-                            <div class="container text-center text-md-start mt-5">
-                                <div class="row mt-3">
-                                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                                        <h6 class="text-uppercase fw-bold">Company name</h6>
-                                        <hr
-                                            class="mb-4 mt-0 d-inline-block mx-auto"
-                                            style="width: 60px; background-color: #7c4dff; height: 2px"
-                                        />
-                                        <p>
-                                            Here you can use rows and columns to organize your footer
-                                            content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                                            elit.
-                                        </p>
-                                    </div>
-
-                                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-
-                                        <h6 class="text-uppercase fw-bold">Products</h6>
-                                        <hr
-                                            class="mb-4 mt-0 d-inline-block mx-auto"
-                                            style="width: 60px; background-color: #7c4dff; height: 2px"
-                                        />
-                                        <p>
-                                            <a href="#!" class="text-white">MDBootstrap</a>
-                                        </p>
-                                        <p>
-                                            <a href="#!" class="text-white">MDWordPress</a>
-                                        </p>
-                                        <p>
-                                            <a href="#!" class="text-white">BrandFlow</a>
-                                        </p>
-                                        <p>
-                                            <a href="#!" class="text-white">Bootstrap Angular</a>
-                                        </p>
-                                    </div>
-
-                                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-
-                                        <h6 class="text-uppercase fw-bold">Useful links</h6>
-                                        <hr
-                                            class="mb-4 mt-0 d-inline-block mx-auto"
-                                            style="width: 60px; background-color: #7c4dff; height: 2px"
-                                        />
-                                        <p>
-                                            <a href="#!" class="text-white">Your Account</a>
-                                        </p>
-                                        <p>
-                                            <a href="#!" class="text-white">Become an Affiliate</a>
-                                        </p>
-                                        <p>
-                                            <a href="#!" class="text-white">Shipping Rates</a>
-                                        </p>
-                                        <p>
-                                            <a href="#!" class="text-white">Help</a>
-                                        </p>
-                                    </div>
-
-                                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-
-                                        <h6 class="text-uppercase fw-bold">Contact</h6>
-                                        <hr
-                                            class="mb-4 mt-0 d-inline-block mx-auto"
-                                            style="width: 60px; background-color: #7c4dff; height: 2px"
-                                        />
-                                        <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-                                        <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-                                        <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                                        <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </section>
-
-                        <div
-                            class="text-center p-3"
-                            style="background-color: rgba(0, 0, 0, 0.2)"
-                        >
-                            © 2020 Copyright:
-                            <a class="text-white" href="https://mdbootstrap.com/"
-                            >MDBootstrap.com</a
-                            >
+        <div>
+            <footer class="text-gray-600 body-font">
+                <div class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
+                    <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
+                        <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+                                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                            </svg>
+                            <span class="ml-3 text-xl">Tailblocks</span>
+                        </a>
+                        <p class="mt-2 text-sm text-gray-500">Air plant banjo lyft occupy retro adaptogen indego</p>
+                    </div>
+                    <div class="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
+                        <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+                            <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+                            <nav class="list-none mb-10">
+                                <li>
+                                    <a class="text-gray-600 hover:text-gray-800">First Link</a>
+                                </li>
+                                <li>
+                                    <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+                                </li>
+                                <li>
+                                    <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+                                </li>
+                                <li>
+                                    <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                                </li>
+                            </nav>
                         </div>
-
-                    </footer>
+                        <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+                            <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+                            <nav class="list-none mb-10">
+                                <li>
+                                    <a class="text-gray-600 hover:text-gray-800">First Link</a>
+                                </li>
+                                <li>
+                                    <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+                                </li>
+                                <li>
+                                    <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+                                </li>
+                                <li>
+                                    <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                                </li>
+                            </nav>
+                        </div>
+                        <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+                            <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+                            <nav class="list-none mb-10">
+                                <li>
+                                    <a class="text-gray-600 hover:text-gray-800">First Link</a>
+                                </li>
+                                <li>
+                                    <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+                                </li>
+                                <li>
+                                    <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+                                </li>
+                                <li>
+                                    <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                                </li>
+                            </nav>
+                        </div>
+                        <div class="lg:w-1/4 md:w-1/2 w-full px-4">
+                            <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CATEGORIES</h2>
+                            <nav class="list-none mb-10">
+                                <li>
+                                    <a class="text-gray-600 hover:text-gray-800">First Link</a>
+                                </li>
+                                <li>
+                                    <a class="text-gray-600 hover:text-gray-800">Second Link</a>
+                                </li>
+                                <li>
+                                    <a class="text-gray-600 hover:text-gray-800">Third Link</a>
+                                </li>
+                                <li>
+                                    <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                                </li>
+                            </nav>
+                        </div>
+                    </div>
                 </div>
+                <div class="bg-gray-100">
+                    <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+                        <p class="text-gray-500 text-sm text-center sm:text-left">© 2020 Tailblocks —
+                            <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" class="text-gray-600 ml-1" target="_blank">@knyttneve</a>
+                        </p>
+                        <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+                            <a class="text-gray-500">
+                                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                                </svg>
+                            </a>
+                            <a class="ml-3 text-gray-500">
+                                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                    <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                                </svg>
+                            </a>
+                            <a class="ml-3 text-gray-500">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                                    <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                                </svg>
+                            </a>
+                            <a class="ml-3 text-gray-500">
+                                <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
+                                    <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
+                                    <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                                </svg>
+                            </a>
+                        </span>
+                    </div>
                 </div>
+            </footer>
+        </div>
 
-            
+
     )
 }
 
