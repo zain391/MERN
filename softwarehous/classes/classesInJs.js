fb5f5734f9a5ed6a4e9768851acf6d93c87e22b2
zain391 <zainqamar391@gmail.com>
2022-09-05 23:52:20 +0500

    working on the classes in the js

---
+++ softwarehous/classes/classesInJs.js
@@ -0,0 +1,27 @@
+class Teacher{
+    constructor(name,subject){
+        this.name=name
+        this.subject=subject
+    }
+}
+let firstTeacher=new Teacher("Sif Rasheed","Network")
+let secTeacher= new Teacher("Sif Usman","Data minnig")
+console.log(firstTeacher);
+console.log(secTeacher);
+
+class std{
+    constructor(name,subject){
+        this.name=name
+        this.subject=subject
+        
+    }
+    doesRun(){
+        console.log("yes zain runs very fast");
+    }
+}
+ 
+let firststd=new std("zain","Network");
+firstTeacher.doesRun
+let secstd= new std("hadi","Data minnig")
+console.log(firststd);
+console.log(secstd);
