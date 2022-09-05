c424b74c890d0dda4c4a9e939534cbdf32cd0972
zain391 <zainqamar391@gmail.com>
2022-09-05 23:57:01 +0500

    we are good to go with class methods in js

--- softwarehous/classes/classesInJs.js
+++ softwarehous/classes/classesInJs.js
@@ -9,7 +9,7 @@ let secTeacher= new Teacher("Sif Usman","Data minnig")
 console.log(firstTeacher);
 console.log(secTeacher);
 
-class std{
+class Std{
     constructor(name,subject){
         this.name=name
         this.subject=subject
@@ -20,8 +20,23 @@ class std{
     }
 }
  
-let firststd=new std("zain","Network");
-firstTeacher.doesRun
-let secstd= new std("hadi","Data minnig")
+let firststd=new Std("zain","Network");
+console.log(firststd.doesRun());
+let secstd= new Std("hadi","Data minnig")
 console.log(firststd);
 console.log(secstd);
+class Car {
+    constructor(name, year) {
+      this.name = name;
+      this.year = year;
+  
+    }
+    age() {
+      let date = new Date();
+      return date.getFullYear() - this.year;
+    }
+  }
+  let myCar = new Car("Ford", 2014);
+  console.log(myCar.age);
+  document.getElementById("demo").innerHTML =
+  "My car is " + myCar.age() + " years old.";
