ae9d0bf5b113a4064e40a27fb295709401e9fe55
zain391 <zainqamar391@gmail.com>
2022-09-06 22:55:04 +0500

    first setup of the StudentReducer with default state value

--- todo/src/redux/reducers/StudentReducer.js
+++ todo/src/redux/reducers/StudentReducer.js
@@ -1,9 +1,30 @@
 import React from 'react'
+let initialState = {
+    stdName: "random1",
+    stdRoll: "123",
+    stdClass: "Class 1",
+    stdBatch: "7th"
+}
+function StudentReducer(state = initialState, action) {
+    // return (
+    //     <div>StudentReducer</div>
+    // )
 
-function StudentReducer() {
-  return (
-    <div>StudentReducer</div>
-  )
+    // first      
+    // switch(action){
+    //     case value:
+    //         break;
+    //     default:
+    //         state    
+    // }
+
+    switch(action.type){
+        case "ADD_STUDENT":
+            // all the logic will be here 
+            return state
+        default:
+           return   state    
+    }
 }
 
 export default StudentReducer
