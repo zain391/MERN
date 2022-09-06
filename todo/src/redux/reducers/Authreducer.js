b471583205196bada83ec8c9fd597faa71acc126
zain391 <zainqamar391@gmail.com>
2022-09-06 23:14:00 +0500

    first confugured the auth reducer

--- todo/src/redux/reducers/Authreducer.js
+++ todo/src/redux/reducers/Authreducer.js
@@ -1,9 +1,37 @@
 import React from 'react'
 
-function Authreducer() {
-  return (
-    <div>Authreducer</div>
-  )
-}
 
+
+let initialState = {
+    isLogIn: true,
+    user:{userName:"zain",email:"zain356@gmail.com"}
+}
+function Authreducer(state = initialState, action) {
+    // return (
+    //     <div>StudentReducer</div>
+    // )
+
+    // first      
+    // switch(action){
+    //     case value:
+    //         break;
+    //     default:
+    //         state    
+    // }
+
+    switch(action.type){
+        case "ISLOGIN":
+            // all the logic will be here 
+            let newStateAfterActionHappens={
+                ...state,
+                // we might had chaged the id or added new attribute in the state and added it in the newstate
+                isLogIn:true,
+                // we might also get some new data from the action (action.payload)
+                newData:action.payload
+            }
+            return newStateAfterActionHappens
+        default:
+           return   state    
+    }
+}
 export default Authreducer
