c17bb761c15528987f7e5f1c26cdd7de8440c53e
zain391 <zainqamar391@gmail.com>
2022-09-06 23:31:08 +0500

    wrapped index.js in provider in order to access the store in all components even in app.js

--- todo/src/redux/rootreducer.js
+++ todo/src/redux/rootreducer.js
@@ -2,7 +2,8 @@ import { combineReducers } from "redux";
 import StudentReducer from "./reducers/StudentReducer";
 import Authreducer from "./reducers/Authreducer";
 const RootReducer=combineReducers({
-
+    StudentReducer,
+    Authreducer
 })
 
 export default RootReducer 
