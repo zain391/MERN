e442165e51223f8f5f9e4ae0e6df9fc75593657b
zain391 <zainqamar391@gmail.com>
2022-09-08 23:52:29 +0500

    working on the logic 3

--- softwarehous/logic/logic.js
+++ softwarehous/logic/logic.js
@@ -3,9 +3,12 @@ let num1=0;
 let num2=0;
 let sortedArray=[];
 for(let i=0; i<array1.length; i++){
-    if(array1[i]<array1[i+1]){
-        num1=array1[i];
-        sortedArray.unshift(num1)
+    if(i=0){
+        for(let j=1; j<array1.length;j++){
+            if(array1[i]<array1[j]){
+                console.log();
+            }
+        }
     }
 }
 
