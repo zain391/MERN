4c8d5908c16dfb1897a439904eb8352996999694
zain391 <zainqamar391@gmail.com>
2022-09-08 23:56:34 +0500

    working on the logic 4

--- softwarehous/logic/logic.js
+++ softwarehous/logic/logic.js
@@ -5,9 +5,13 @@ let sortedArray=[];
 for(let i=0; i<array1.length; i++){
     if(i=0){
         for(let j=1; j<array1.length;j++){
-            if(array1[i]<array1[j]){
-                console.log();
-            }
+            // if(array1[i]<array1[j]){
+            //     console.log("sdklafjdlsk")
+            //     sortedArray.unshift(array1[i])
+            // }
+            // else{
+            //     sortedArray.unshift(array1[j])
+            // }
         }
     }
 }
