0d4ea55b9480ab541d3b24838fddcb0c0b3b4036
zain391 <zainqamar391@gmail.com>
2022-09-11 11:35:20 +0500

    learned the switch

---
+++ softwarehous/switch/switch.js
@@ -0,0 +1,16 @@
+let num=45;
+
+switch(num){
+    case 40:
+        console.log("less than num");
+    case 50:
+        console.log("greater than num");
+        break;
+    case 45 :
+        console.log("equal to num");
+        break;
+      default:
+        console.log("nun");  
+}
+
+
